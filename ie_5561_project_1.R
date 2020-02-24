4242b77016e65245f9feddd74d75a108efc5fe97
aroberts394 <aroberts394@gmail.com>
2020-02-24 14:14:13 -0600

    starting EDA on higher ed data

--- ie_5561_project_1.R
+++ ie_5561_project_1.R
@@ -1,2 +1,66 @@
 ## IE 5561 Project 1 ##
-#Info on how to use Rstudio with Git and Github here: https://happygitwithr.com/rstudio-git-github.html
+
+# install required packages
+install.packages('TH.data')
+install.packages("tidyverse")
+install.packages("caret")
+install.packages("funModeling")
+install.packages("corrplot")
+
+# load required libraries
+require(TH.data)      # for glaucoma data set
+require(tidyverse)    # for data manipulation and graphics 
+require(caret)        # for resampling and model training
+require(funModeling)  # for useful data exploration tools
+require(corrplot)     # for correlation matrices
+
+
+                      ### Exploratory Data Analysis ###
+# load in dataset
+higher_ed <- read_csv("/Volumes/GoogleDrive/My Drive/IE 5561 Projects/Project 1/Data/Higher_Ed.csv", col_types = cols(BIRYR = col_factor(),                                                                     GENDER = col_factor(),
+                                  MINRTY = col_factor(),                                                                    RACETH = col_factor(), 
+                                  DGRDG = col_factor(),
+                                  OCEDRLP = col_factor(),
+                                  NOCPRMG = col_factor()))
+# rename the dataset
+ed <- higher_ed
+# attach the Glaucoma dataset to the environment
+attach(ed)
+# check for missing values
+sum(is.na(ed))
+# create a validation set 70% for training set
+set.seed(123) 
+validation_index <- createDataPartition(ed$SALARY, p = 0.7, list = FALSE)
+# select 20% of the data for validation
+validation <- ed[-validation_index,]
+# use the remaining 80% of data to training and testing the models
+ed <- ed[validation_index,]
+# display the structure of the data
+str(ed)
+# dimensions of dataset
+dim(ed)
+#check frequency of the factor variables
+freq(ed)
+# summarize all attribute distributions
+summary(ed)
+#check distributions of the variables
+plot_num(ed)
+# split input and output
+x <- ed[,1:10]
+y <- ed[,11]
+
+# visualize salary wrt to gender, minority class and race
+ggplot(ed, aes(x = GENDER, y = SALARY)) +
+  geom_boxplot() +
+  scale_y_continuous(labels = scales::dollar) +
+  coord_flip()
+ggplot(ed, aes(x = MINRTY, y = SALARY)) +
+  geom_boxplot() +
+  scale_y_continuous(labels = scales::dollar) +
+  coord_flip()
+ggplot(ed, aes(x = RACETH, y = SALARY)) +
+  geom_boxplot() +
+  scale_y_continuous(labels = scales::dollar) +
+  coord_flip()
+
+
