ce7af549a92dd07afb68b85892d1f599acd8d95a
aroberts394 <aroberts394@gmail.com>
2020-02-24 21:12:16 -0600

    made some brief changes

--- ie_5561_project_1.R
+++ ie_5561_project_1.R
@@ -25,10 +25,23 @@ higher_ed <- read_csv("/Volumes/GoogleDrive/My Drive/IE 5561 Projects/Project 1/
                                        DGRDG = col_factor(),
                                        OCEDRLP = col_factor(),
                                        NOCPRMG = col_factor()))
+
+
 # rename the dataset
 ed <- higher_ed
+
 # attach the Glaucoma dataset to the environment
 attach(ed)
+
+# rename factor levels
+ed$GENDER <- plyr::revalue(GENDER, c("1" = "Female", "2" = "Male"))
+ed$MINRTY <- plyr::revalue(MINRTY, c("0" = "No", "1" = "Yes"))
+ed$RACETH <- plyr::revalue(RACETH, c("1" = "Asian", "2" = "White", "3" = "Minorities", "4"  = "Other"))
+ed$DGRDG <-  plyr::revalue(DGRDG, c("1" = "Bachelors", "2" = "Masters", "3" = "Doctorate", "4" = "Professional"))
+ed$OCEDRLP <- plyr::revalue(OCEDRLP, c("1" = "Closely related", "2" = "Somewhat related", "3" = "Not related"))
+ed$NOCPRMG <- plyr::revalue(NOCPRMG, c("1" = "Computer Scientists", "2" = "Life Scientists", "3" = "Physical scientists", "4" = "Social Scientists", "5" = "Engineers", "6" = "Science Related", "7" = "Non-Scientists"))
+
+
 # check for missing values
 sum(is.na(ed))
 
@@ -39,7 +52,7 @@ validation_index <- createDataPartition(ed$SALARY, p = 0.7, list = FALSE)
 # select 20% of the data for validation
 validation <- ed[-validation_index,]
 
-# use the remaining 80% of data to training and testing the models
+# use the remaining 70% of data to training and testing the models
 ed <- ed[validation_index,]
 
 # display the structure of the data
@@ -54,22 +67,27 @@ freq(ed)
 # summarize all attribute distributions
 summary(ed)
 
-#check distributions of the variables
+#visualize distributions of the continous variables
 plot_num(ed)
 
 # split input and output
 x <- ed[,1:10]
 y <- ed[,11]
 
-# visualize salary wrt to gender, minority class and race
+# exploring categorical variables
+# visualize salary wrt to gender, minority factor, race, job type
+ggplot(ed, aes(SALARY)) +
+  geom_histogram()
+
 ed %>%
   group_by(GENDER) %>%
   summarise(
     avg_sal = mean(SALARY),
     sd_sal  = sd(SALARY),
+    med_sal = median(SALARY),
     n       = n()
   )
-ggplot(ed, aes(x = GENDER, y = SALARY)) +
+p1 <- ggplot(ed, aes(x = GENDER, y = SALARY)) +
   geom_boxplot() +
   scale_y_continuous(labels = scales::dollar) +
   coord_flip()
@@ -79,12 +97,12 @@ ed %>%
   summarise(
     avg_sal = mean(SALARY),
     sd_sal  = sd(SALARY),
+    med_sal = median(SALARY),
     n       = n()
   )
-ggplot(ed, aes(x = MINRTY, y = SALARY)) +
+p2 <- ggplot(ed, aes(x = MINRTY, y = SALARY)) +
   geom_boxplot() +
-  scale_y_continuous(labels = scales::dollar) +
-  coord_flip()
+  scale_y_continuous(labels = scales::dollar)
 
 ed %>%
   group_by(RACETH) %>%
@@ -94,13 +112,55 @@ ed %>%
     n       = n()
   )
 
-ggplot(ed, aes(x = RACETH, y = SALARY)) +
+p3 <- ggplot(ed, aes(x = RACETH, y = SALARY)) +
   geom_boxplot() +
   scale_y_continuous(labels = scales::dollar) +
   coord_flip()
 
+ed %>%
+  group_by(NOCPRMG) %>%
+  summarise(
+    avg_sal = mean(SALARY),
+    sd_sal  = sd(SALARY),
+    n       = n()
+  )
+p4 <- ggplot(ed, aes(x = NOCPRMG, y = SALARY)) +
+  geom_boxplot() +
+  scale_y_continuous(labels = scales::dollar) +
+  coord_flip()
+
+gridExtra::grid.arrange(p1, p2, p3, p4, nrow = 2)
+
+# assessing normality assumption of salary
+par(mfrow = c(1, 2))
+# non-log transformed
+qqnorm(SALARY, main = "Untransformed\nNormal Q-Q Plot")
+qqline(SALARY)
+# log transformed
+qqnorm(log(SALARY), main = "Log Transformed\nNormal Q-Q Plot")
+qqline(log(SALARY))
+
+fm <- lm(SALARY~., data = ed)
+summary(fm)
+
+engineers <- ed %>%
+  filter(NOCPRMG == "Engineers")
+
+ggplot(engineers, aes(GENDER, SALARY)) + 
+  geom_boxplot()
+
+engineers %>%
+  group_by(GENDER) %>%
+  summarise(
+    avg_sal = mean(SALARY),
+    sd_sal  = sd(SALARY),
+    med_sal = median(SALARY),
+    n       = n()
+  )
+
 
                       ### DATA PREPROCESSING ###
+
 
 
                       ### MODEL BUILDING AND EVALUATION ###
